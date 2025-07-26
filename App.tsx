0fe691318eede191e4eefc370da9c3e0a74fbbc9
c1op <c1op@proton.me>
2025-07-26 23:44:41 +0200

    Initial commit: Tailwind setup

---
+++ App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <main className="min-h-screen flex items-center justify-center bg-neutral-900 text-white">
+        <h1 className="text-4xl font-bold">Lofiglow is working ✨</h1>
+      </main>
+    );
+  }
+  
+  export default App;
+  
